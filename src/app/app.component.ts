7d4e9906afcd12dc43be6b7ea395bd3ca6c616e0
anggara setiawan <aanggaadc@gmail.com>
2025-03-11 01:35:20 +0700

    import image upload component to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { ImageUploadComponent } from './components/image-upload/image-upload.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [ImageUploadComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'client';
